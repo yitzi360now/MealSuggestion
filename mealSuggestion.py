76e6fa001e785cfff8eba860f00b0858f723085b
yitzi360now <yitzi.jacobs@fitness360now.com>
2021-06-15 15:03:04 +0300

    added dbConnect.py

--- mealSuggestion.py
+++ mealSuggestion.py
@@ -1,17 +1,9 @@
-import mysql.connector as mysql
+import dbConnect
 
-db = mysql.connect(
-    host="localhost",
-    user="root",
-    passwd="matthews34",
-    database="fitness360db"
-)
-
-cursor = db.cursor()
+cursor = dbConnect.db.cursor()
 query = "select * from fitness360db.groups"
 cursor.execute(query)
 tables = cursor.fetchall()
 for table in tables:
     print(table)
 
-# test2
