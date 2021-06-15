0062be47748b4b4bb4fc76b207ddbb9b15e1df9a
yitzi360now <yitzi9@hotmail.com>
2021-06-15 14:41:10 +0300

    connected to MySQL

--- mealSuggestion.py
+++ mealSuggestion.py
@@ -1,1 +1,15 @@
+import mysql.connector as mysql
 
+db = mysql.connect(
+    host="localhost",
+    user="root",
+    passwd="matthews34",
+    database="fitness360db"
+)
+
+cursor = db.cursor()
+query = "select * from fitness360db.groups"
+cursor.execute(query)
+tables = cursor.fetchall()
+for table in tables:
+    print(table)
