dac12f5362765757a1b75871b38baba0528f0381
yitzi360now <yitzi.jacobs@fitness360now.com>
2021-06-16 18:10:25 +0300

    added gitignore

--- mealSuggestion.py
+++ mealSuggestion.py
@@ -1,4 +1,5 @@
 import dbConnect
+import socket
 
 cursor = dbConnect.db.cursor()
 query = "select * from fitness360db.groups"
@@ -7,3 +8,12 @@ tables = cursor.fetchall()
 for table in tables:
     print(table)
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect to an IP with Port, could be a URL
+sock.connect(('0.0.0.0', 8080))
+# Send some data, this method can be called multiple times
+sock.send("Twenty-five bytes to send")
+# Receive up to 4096 bytes from a peer
+sock.recv(4096)
+# Close the socket connection, no more data transmission
+sock.close()
