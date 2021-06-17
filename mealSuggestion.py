4a138bcf8d2ac9de18fcc72f2f9304c90b51beb1
yitzi360now <yitzi.jacobs@fitness360now.com>
2021-06-17 12:34:57 +0300

    finished sql query

--- mealSuggestion.py
+++ mealSuggestion.py
@@ -1,19 +1,17 @@
+import datetime
 import dbConnect
-import socket
 
+# database connection
 cursor = dbConnect.db.cursor()
-query = "select * from fitness360db.groups"
-cursor.execute(query)
-tables = cursor.fetchall()
-for table in tables:
-    print(table)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connect to an IP with Port, could be a URL
-sock.connect(('0.0.0.0', 8080))
-# Send some data, this method can be called multiple times
-sock.send("Twenty-five bytes to send")
-# Receive up to 4096 bytes from a peer
-sock.recv(4096)
-# Close the socket connection, no more data transmission
-sock.close()
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+email = 'halfonamir1@gmail.com'
+query = "select meal_id,meal_date,meal_hour,day_of_week from fitness360db.meals where user_email = '" + email + "'"
+cursor.execute(query)
+mealTimes = cursor.fetchall()
+for meal in mealTimes:
+    print(meal)
+
+
