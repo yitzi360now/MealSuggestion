868fec05343ab92e8ec16e22b898208ab63f76e4
yitzi360now <yitzi.jacobs@fitness360now.com>
2021-07-27 16:06:13 +0300

    added calorie target

--- mealSuggestion.py
+++ mealSuggestion.py
@@ -6,8 +6,16 @@ import dbConnect
 cursor = dbConnect.db.cursor()
 
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
 FMT = '%H:%M:%S'
+current_time = now.strftime(FMT)
+hour = int(current_time[0:2])
+if 6 <= hour < 12:
+    partOfDay = 1
+elif 12 <= hour < 18:
+    partOfDay = 2
+else:
+    partOfDay = 3
+
 weekday = datetime.today().weekday()
 if weekday == 6:
     weekday = 1
@@ -23,17 +31,29 @@ app = Flask(__name__)
 # user = request.args.get('user')
 email = 'yitzi9@hotmail.com'
 
-# get relevant data about user
-query = "select age,weight,gender from fitness360db.users where email='" + email + "'"
+# get relevant data about user. General info:
+query = "select age,weight,gender,height from fitness360db.users where email='" + email + "'"
 cursor.execute(query)
 mealInfo = cursor.fetchall()
 userAge = mealInfo[0][0]
 userWeight = mealInfo[0][1]
 userGender = mealInfo[0][2]
-print(userAge, userWeight, userGender)
+userHeight = mealInfo[0][3]
+# calorie intake so far today:
+query = "select SUM(calories) from fitness360db.meals where user_email='" + email + "' and meal_date = curdate()"
+cursor.execute(query)
+caloriesYetToday = cursor.fetchall()[0][0]
+
+# calculate calorie target for meal
+if userGender:
+    calorieTarget = (655 + 1.8 * userHeight + 9.6 * userWeight - 4.7 * userAge) * partOfDay/3 - caloriesYetToday
+else:
+    calorieTarget = (66 + 5 * userHeight + 13.7 * userWeight - 6.8 * userAge) * partOfDay/3 - caloriesYetToday
+
+print(userAge, userWeight, userGender, userHeight, calorieTarget, caloriesYetToday)
 
 # get relevant meal data about all user's meals in database
-query = "select email,age,weight,meal_id,meal_date,meal_hour,weekday,gender from fitness360db.meals" \
+query = "select age,weight,meal_id,meal_hour,weekday,gender,calories from fitness360db.meals" \
         " join fitness360db.users ON (fitness360db.meals.user_email=fitness360db.users.email)"
 cursor.execute(query)
 mealTimes = cursor.fetchall()
@@ -45,33 +65,35 @@ minDiff2 = 99999999999999
 minID2 = 0
 for meal in mealTimes:
     # calculate difference between current time and time of meal
-    hour = meal[5] + ':00'
+    hour = meal[3] + ':00'
     # print(hour)
     timeDiff1 = datetime.strptime(current_time, FMT) - datetime.strptime(hour, FMT)
     timeDiff2 = datetime.strptime(hour, FMT) - datetime.strptime(current_time, FMT)
     timeDiff = min(timeDiff1.seconds // 60, timeDiff2.seconds // 60)
 
     # calculate difference between current weekday and day of meal where each day has its own value
-    weekdayDiff1 = abs(weekday - meal[6])
+    weekdayDiff1 = abs(weekday - meal[4])
     weekdayDiff2 = 7 - weekdayDiff1
     weekdayDiffA = min(weekdayDiff1, weekdayDiff2)
     # print(weekdayDiffA)
 
     # calculate difference in users data vs user form database
-    ageDiff = abs(userAge - meal[1])
-    genderDiff = abs(userGender - meal[7])
-    weightDiff = abs(userWeight - meal[2])
+    ageDiff = abs(userAge - meal[0])
+    genderDiff = abs(userGender - meal[5])
+    weightDiff = abs(userWeight - meal[1])
     personDiff = ageDiff * 10 + genderDiff * 1000 + weightDiff * 20
 
+    # calculate how close meal is to target calorie wise
+    calorieDiff = abs(calorieTarget - meal[6])
+
     # calculate overall difference and save meal id if it is the closest yet
-    difference1 = weekdayDiffA * 1000 + timeDiff
-    difference1 += personDiff
+    difference1 = weekdayDiffA * 1000 + timeDiff + personDiff * 2 + calorieDiff * 2
     if difference1 < minDiff1:
         minDiff1 = difference1
-        minID1 = meal[3]
+        minID1 = meal[2]
 
     # calculate difference between current weekday and day of meal where each day has binary value of weekend or not
-    theDay = int(meal[6])
+    theDay = int(meal[4])
     if theDay < 6:
         theDay = 0
     else:
@@ -84,11 +106,10 @@ for meal in mealTimes:
     # print(weekdayDiffB)
 
     # calculate overall difference and save meal id if it is the closest yet
-    difference2 = weekdayDiffB * 1000 + timeDiff
-    difference2 += personDiff
+    difference2 = weekdayDiffB * 1000 + timeDiff + personDiff * 2 + calorieDiff * 2
     if difference2 < minDiff2:
         minDiff2 = difference2
-        minID2 = meal[3]
+        minID2 = meal[4]
 
 # print results based on first algorithm
 query = "select * from fitness360db.meals where meal_id =" + str(minID1)
